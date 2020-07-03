1a66009f614819bbb1ffce81be8d2efe3addaf60
acvp3lla <lucinaogordon@hotmail.com>
2020-07-03 17:07:24 -0500

    Updates

---
+++ precache-manifest.f9be1f370de6831ee0b608495bd8f824.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd24258e3697bf96f223c8c232849784",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc2040aecdcce9336413",
+    "url": "/static/css/main.0cd01124.chunk.css"
+  },
+  {
+    "revision": "1b313e3fb8111d65a976",
+    "url": "/static/js/2.86af5efe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.86af5efe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc2040aecdcce9336413",
+    "url": "/static/js/main.38944fee.chunk.js"
+  },
+  {
+    "revision": "12e134867301281bc2d5",
+    "url": "/static/js/runtime-main.a471abdf.js"
+  },
+  {
+    "revision": "62868fc0ddf5074abda68203e91612d1",
+    "url": "/static/media/photo1.62868fc0.jpg"
+  },
+  {
+    "revision": "df6e11b46b02702a1d62811138fecd71",
+    "url": "/static/media/photo2.df6e11b4.jpg"
+  },
+  {
+    "revision": "5d8cb019bd60a19cacecaf3b1a91ceea",
+    "url": "/static/media/photo3.5d8cb019.png"
+  },
+  {
+    "revision": "2cb2b9c69f36366327a18d170c866819",
+    "url": "/static/media/photo4.2cb2b9c6.jpg"
+  },
+  {
+    "revision": "d97cb3d0f508c936df5193c85098af7b",
+    "url": "/static/media/search-icon.d97cb3d0.png"
+  },
+  {
+    "revision": "0dd4fd84e5fbf8ca1382199b7f5c7f9f",
+    "url": "/static/media/solo-logo.0dd4fd84.png"
+  },
+  {
+    "revision": "4c1786ae4a87cee1f33db3f81e983cec",
+    "url": "/static/media/solo-logo.4c1786ae.png"
+  }
+]);
